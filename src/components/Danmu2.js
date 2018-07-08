4f907d44ef4fdb002658eb608fd54cfdadf9ec07
gitovers <yjatts@163.com>
2018-07-09 00:00:42 +0800

    x

--- src/components/Danmu2.js
+++ src/components/Danmu2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Endless from 'endless-react'
+// import Endless from 'endless-react'
 import './App.css'
 
 
@@ -7,6 +7,12 @@ import './App.css'
 - Use a queue to scroll.new item enter queue,once it's displayed, then dequeue.
 - search, endless scroll
 - 如果连续的话,那每个对话显示的时间长短就不一样了.而且对话与对话之间连成一片,不知道什么时候是新对话.
+
+- https://github.com/skorekm/EndlessScroll-React 实现了基本的下拉加载功能,文档超级详细,用起来很贴心,适合学习.
+- https://github.com/scrollback/endless 功能正是我需要的,但是版本太老了,需要自己重新实现一遍.
+
+- 加载的同时删除
+- 自动滚动
 */
 export default class Danmu extends Component {
   constructor(props) {
@@ -20,8 +26,8 @@ export default class Danmu extends Component {
   render() {
     return (
       <div className="danmu-board">
-        {/* <div className="scroll-bar">xxxxxx</div> */}
-        <Endless items={this.props.danmuList} onScroll={this.onScroll} />
+        <div className="scroll-bar">xxxxxx</div>
+        {/* <Endless items={this.props.danmuList} onScroll={this.onScroll} /> */}
       </div >
     )
   }
